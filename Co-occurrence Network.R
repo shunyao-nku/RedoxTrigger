bf99d3d13bd9ae1adfee74562fa726fe4ea1ad9b
shunyao-nku <106733824+shunyao-nku@users.noreply.github.com>
2022-06-02 18:16:31 +0800

    Update Co-occurrence Network.R

--- Co-occurrence Network.R
+++ Co-occurrence Network.R
@@ -35,93 +35,52 @@ occor=corr.test(OTU,
      adjust="fdr",
      alpha=0.05)
 
-####数据量小时可以用psych包corr.test求相关性矩阵，数据量大时，可应用WGCNA中corAndPvalue,但p值需要借助其他函数矫正
-occor<-corr.test(otu,use="pairwise",method="spearman",adjust="fdr",alpha=.05)
-occor.r<-occor$r ###取相关性矩阵R值
-occor.p<-occor$p ###取相关性矩阵p值
-###确定物种间存在相互作用关系的阈值，将相关性R矩阵内不符合的数据转换为0
-occor.r[occor.p>0.05|abs(occor.r)<0.6]<-0 
+# Acquire correaltion coefficients r and p values
+r_matrix=occor$r
+p_matrix=occor$p
 
-###构建igraph对象
+r_matrix[p_matrix>p.cutoff|abs(r_matrix)<r.cutoff]=0
+p_value=melt(p_matrix)
+r_value=melt(r_matrix)
+r_value=cbind(r_value, p_value$value)
+r_value=subset(r_value, r_value[,3]!=0)
+r_value=na.omit(r_value)
+abs=abs(r_value$value)
+
+# Add positive or negative correaltion information
+linktype=r_value$value
+linktype[linktype>0]=1
+linktype[linktype<0]=-1
+r_value=cbind(r_value, abs, linktype)
+names(r_value) <- c("Source","Target","r_value","p_value", "abs_value", "linktype")
+names(p_value) <- c("Source","Target","p_value")
+
+# Output edge data used for co-occurrence network construction in Gephi or Cytoscape
+write.csv(r_value,file="D://Zhenlai_rewrite/Subnetwork/PW8910/Edges.csv", row.names=FALSE)
+
+node_OTU <- as.data.frame(as.data.frame(r_value[,1])[!duplicated(as.data.frame(r_value[,1])), ])
+node_Ev <- as.data.frame(as.data.frame(r_value[,2])[!duplicated(as.data.frame(r_value[,2])), ])
+names(node_OTU)="Id"
+names(node_Ev)="Id"
+list <- rbind(node_Ev, node_OTU)
+list <- rbind(node_OTU)
+list=subset(tax,Id %in% list$Id)
+list$Label <- list$Id
+
+# Output node data used for co-occurrence network construction in Gephi or Cytoscape
+write.csv(list,file="D://Zhenlai_rewrite/Subnetwork/PW8910/Nodes.csv", row.names=FALSE)
+
+
+
+###igraph
 igraph<-graph_from_adjacency_matrix(occor.r,mode="undirected",weighted=TRUE,diag=FALSE)
 igraph
-###NOTE:可以设置weighted=NULL,但是此时要注意此函数只能识别相互作用矩阵内正整数，所以应用前请确保矩阵正确。
-###可以按下面命令转换数据
-#occor.r[occor.r!=0]<- 1
-#igraph<-graph_from_adjacency_matrix(occor.r,mode="undirected",weighted=NULL,diag=FALSE)
-
-###是否去掉孤立顶点，根据自己实验而定
-# remove isolated nodes，即去掉和所有otu均无相关性的otu 可省略，前期矩阵已处理过
+# remove isolated nodes
 bad.vs<-V(igraph)[degree(igraph) == 0]
 igraph<-delete.vertices(igraph, bad.vs)
 igraph
 
-###将igraph weight属性赋值到igraph.weight
-igraph.weight<-E(igraph)$weight
 
-###做图前去掉igraph的weight权重，因为做图时某些layout会受到其影响
-E(igraph)$weight<-NA
-###简单出图
-#设定随机种子数，后续出图都从同一随机种子数出发，保证前后出图形状相对应
-set.seed(123)
-plot(igraph,main="Co-occurrence network",vertex.frame.color=NA,vertex.label=NA,edge.width=1,
-     vertex.size=5,edge.lty=1,edge.curved=TRUE,margin=c(0,0,0,0))
-
-
-##如果构建网络时，weighted=NULL,此步骤不能统计
-sum(igraph.weight>0)###number of postive correlation
-sum(igraph.weight<0)###number of negative correlation
-
-#set edge color，postive correlation 设定为red, negative correlation设定为blue
-E.color<-igraph.weight
-E.color[E.color>0]<-"red"
-E.color[E.color<0]<-"blue"
-E(igraph)$color <-as.character(E.color)
-
-
-###改变edge颜色后出图
-set.seed(123)
-plot(igraph,main="Co-occurrence network",vertex.frame.color=NA,vertex.label=NA,edge.width=1,
-     vertex.size=5,edge.lty=1,edge.curved=TRUE,margin=c(0,0,0,0))
-
-
-##可以设定edge的宽度，例如将相关系数与edge width关联
-#set edge width
-E(igraph)$width<-abs(igraph.weight)*4
-
-###改变edge宽度后出图
-set.seed(123)
-plot(igraph,main="Co-occurrence network",vertex.frame.color=NA,vertex.label=NA,
-     vertex.size=5,edge.lty=1,edge.curved=TRUE,margin=c(0,0,0,0))
-
-
-###另外可以设置vertices size, vertices color来表征更多维度的数据
-####注意otu_pro.txt文件为我随机产生的数据，因此网络图可能不会产生特定的模式或规律。
-otu_pro<-read.table("otu_pro.txt",head=T,row.names=1)
-#set vertices size
-igraph.size<-otu_pro[V(igraph)$name,]
-igraph.size1<-log((igraph.size$abundance)*100)
-V(igraph)$size<-igraph.size1
-
-#set vertices color
-igraph.col<-otu_pro[V(igraph)$name,]
-levels(igraph.col$phylum)
-levels(igraph.col$phylum)<-c("green","deeppink","deepskyblue","yellow","brown","pink","gray","cyan","peachpuff")
-V(igraph)$color <- as.character(igraph.col$phylum)
-
-set.seed(123)
-plot(igraph,main="Co-occurrence network",vertex.frame.color=NA,vertex.label=NA,
-     edge.lty=1,edge.curved=TRUE,margin=c(0,0,0,0))
-
-
-####改变layout,layout有很多，具体查看igraph官方帮助文档。
-set.seed(123)
-plot(igraph,main="Co-occurrence network",layout=layout_with_kk,vertex.frame.color=NA,vertex.label=NA,
-     edge.lty=1,edge.curved=TRUE,margin=c(0,0,0,0))
-
-set.seed(123)
-plot(igraph,main="Co-occurrence network",layout=layout.fruchterman.reingold,vertex.frame.color=NA,vertex.label=NA,
-     edge.lty=1,edge.curved=TRUE,margin=c(0,0,0,0))
 
 
 ###network property
